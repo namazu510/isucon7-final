9bb3dee542e5230d8f899850bbd19584daaa3b07
yuuki <yuuki0xff@gmail.com>
2017-11-25 10:54:58 +0900

    現在時刻をアプリが稼働しているサーバから取得するよう変更
    サーバ間の時刻が揃っているので、このようなことをしても問題ないはず

--- go/src/app/game.go
+++ go/src/app/game.go
@@ -149,10 +149,7 @@ func big2exp(n *big.Int) Exponential {
 
 func getCurrentTime() (int64, error) {
 	var currentTime int64
-	err := db.Get(&currentTime, "SELECT floor(unix_timestamp(current_timestamp(3))*1000)")
-	if err != nil {
-		return 0, err
-	}
+	currentTime = time.Now().Unix()
 	return currentTime, nil
 }
 
